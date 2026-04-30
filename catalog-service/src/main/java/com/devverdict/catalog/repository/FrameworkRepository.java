5cabdf27cf59637c7895860e679336cc3ec312a3
Mc Eddy Jeune <mceddy95@hotmail.fr>
2026-04-29 22:02:15 -0400

    Revert "Phase 3: API Gateway"

--- catalog-service/src/main/java/com/devverdict/catalog/repository/FrameworkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devverdict.catalog.repository;
-
-import com.devverdict.catalog.domain.Framework;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FrameworkRepository extends JpaRepository<Framework, Long> {
-}
